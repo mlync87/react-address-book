91f19ed77b4f6c5d90cd3557ce6d04b06f0c21e6
mlync87 <martylynch35@hotmail.com>
2022-11-21 11:08:05 +0000

    app.js updated, other compnents to follow

--- src/App.js
+++ src/App.js
@@ -1,30 +1,55 @@
-import { useEffect, useState } from "react"
-import { Link, Route, Routes } from "react-router-dom"
-import ContactsList from "./components/ContactsList"
-import ContactsAdd from "./components/ContactsAdd"
-import ContactsView from "./components/ContactsView"
-import "./styles/styles.css"
+import { useEffect, useState } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import ContactsList from "./components/ContactsList";
+import ContactsAdd from "./components/ContactsAdd";
+import ContactsView from "./components/ContactsView";
+import "./styles/styles.css";
 
 export default function App() {
-  const [contacts, setContacts] = useState([])
-  
+  const [contacts, setContacts] = useState([]);
+
   //TODO: Load all contacts on useEffect when component first renders
+  useEffect(() => {
+    // use a fetch request to the local server being used
+    fetch("http://localhost:3000/contacts")
+      .then((res) => res.json())
+      .then((data) => {
+        // console log the result
+        console.log("contact data", data);
+        setContacts(data);
+      });
+  }, []);
 
   return (
     <>
       <nav>
         <h2>Menu</h2>
         <ul>
+          {/* create links to specified items and show text displayed */}
           {/* TODO: Make these links */}
-          <li>Contacts List</li>
-          <li>Add New Contact</li>
+          <li>
+            {/* the "/" is the main area of the page */}
+            <Link to="/">Contacts List</Link>
+          </li>
+          <li>
+            <Link to="/addNewContact">Add New Contact</Link>
+          </li>
         </ul>
       </nav>
       <main>
+        {/* create routes to retrieve and display data from other sections when the link is clicked */}
         <Routes>
           {/* TODO: Add routes here  */}
+          <Route path="/" element={<ContactsList contacts={contacts} />} />
+          <Route
+            path="/addNewContact"
+            element={
+              <ContactsAdd contacts={contacts} setContacts={setContacts} />
+            }
+          />
+          <Route path="/contacts/:id" element={<ContactsView />} />
         </Routes>
       </main>
     </>
-  )
+  );
 }
