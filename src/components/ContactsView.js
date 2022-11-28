6356349bc0842f540e558e39d228545ebadecd08
mlync87 <martylynch35@hotmail.com>
2022-11-28 08:48:47 +0000

    contactsView updated

--- src/components/ContactsView.js
+++ src/components/ContactsView.js
@@ -1,23 +1,23 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 function ContactsView() {
   const [contact, setContact] = useState(false);
 
   const { id } = useParams();
 
-  useEffect(() => {
+  useEffect(async () => {
     // use a fetch request to displat the contact and ID
     // according to parameter changes
-    fetch(`http://localhost:4000/contacts/${id}`)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log("data", data);
-        setContact(data);
-      });
+    // interesting thing about await, it can only be used in an async
+    const res = await fetch(`http://localhost:4000/contacts/${id}`);
+    const data = await res.json();
+
+    setContact(data);
   }, []);
 
   if (!contact) {
+    // return Loading while there is no contact present
     return <p>Loading</p>;
   }
 
@@ -25,16 +25,21 @@ function ContactsView() {
     <div>
       {/* display full name as a header whenshowing contact details */}
       <h2>
-        {contact.firstName} {contact.lastName}
+        {/* request the type on contact to be returned also */}
+        {contact.firstName} {contact.lastName} ({contact.type})
       </h2>
       {/* address, email, linkedIn and twitter will appear in a vertical
 row beneath individuals name */}
       <p>
-        {contact.street} {contact.city}
+        <a href={contact.linkedIn}>LinkedIn</a> |
+        <a href={contact.twitter}>Twitter</a>
       </p>
-      <p>{contact.email}</p>
-      <p>LinkedIn: {contact.linkedIn}</p>
-      <p>Twitter: {contact.twitter}</p>
+      <p>email: {contact.email}</p>
+      <p>
+        address: {contact.street} {contact.city}
+      </p>
+
+      <Link to={`/contacts/${contact.id}/meetings`}>Meetings</Link>
     </div>
   );
 }
